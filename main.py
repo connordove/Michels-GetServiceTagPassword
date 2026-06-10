08453703e9711fb13e7a393bf75ea6c9071f7d34
cdove <connor0233@icloud.com>
2026-06-10 08:37:07 -0500

    user can press enter to submit! also added comments and prevents program from running blank service tags

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import tkinter as tk
-import subprocess
-import re
 import os
-from os.path import join
+import re
+import subprocess
+import tkinter as tk
 
 root = tk.Tk()
 
@@ -11,23 +10,29 @@ root.geometry("600x400")
 
 # gets the userprofile name, ex: cdove
 os.environ["USERPROFILE"]
-# sets the file_path to the users desktop
+# sets the file_path to the users desktop, prints path in terminal
 desktop = os.path.join(os.environ["USERPROFILE"], "OneDrive - Michels Corporation", "Desktop")
 file_path = os.path.join(desktop, "LAPSHistory.txt")
+print("History File Path is : " + file_path)
 
 # declaring string variable
 # for storing service tag and password
 service_tag = tk.StringVar()
 st_password = tk.StringVar()
 password_display = tk.StringVar()
+# sets password_display label to display following text
 password_display.set("Password will appear here")
 
 
 # defining a function that will
 # get the name and password and
 # print them on the screen
+# save the name and password to file
 def submit(event=None):
     st = service_tag.get()
+    if st == "":
+        print("Service Tag not found.")
+        return
 
     print("The service tag is : " + st)
 
@@ -50,7 +55,6 @@ def submit(event=None):
         print("Password not found.")
         password_display.set("Password not found.")
     try:
-        print(file_path)
         with open(file_path, "a", encoding="utf-8") as f:
             f.write(f"{st}, {st_password}\n")
     except:
@@ -58,11 +62,14 @@ def submit(event=None):
 
     load_history()
 
-    service_tag.set("")
+    service_tag.set("INVALID")
 
+# defining a function that will
+# load name and passwords from
+# the given file path and display in
+# history_box Listbox
 def load_history():
     try:
-        print(file_path)
         with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
@@ -84,8 +91,10 @@ service_tag_label = tk.Label(root, text='Service Tag', font=('calibre', 10, 'bol
 # name using widget Entry
 service_tag_entry = tk.Entry(root, textvariable=service_tag, font=('calibre', 10, 'normal'))
 
+# allows the user to press enter to run submit function
 service_tag_entry.bind("<Return>", submit)
 
+# creating a label for password
 password_label = tk.Label(root, textvariable=password_display, font=('calibre', 10, 'bold'))
 password_label.grid(row=4, column=0)
 
@@ -93,12 +102,13 @@ password_label.grid(row=4, column=0)
 # Button that will call the submit function
 sub_btn = tk.Button(root, text='Submit', command=submit)
 
+# creating a listbox to display service tag and password history
 history_box = tk.Listbox(root, height=10, width=50, font=(';calibre', 10, 'bold'))
 history_box.grid(row=5, column=0, columnspan=2)
 
+# creating a scroll bar to view large amounts of st and password history
 scrollbar = tk.Scrollbar(root, command=history_box.yview)
 history_box.config(yscrollcommand=scrollbar.set)
-
 scrollbar.grid(row=5, column=2, sticky='ns')
 
 # placing the label and entry in
@@ -108,6 +118,7 @@ service_tag_label.grid(row=0, column=0)
 service_tag_entry.grid(row=0, column=1)
 sub_btn.grid(row=2, column=1)
 
+# loads the history
 load_history()
 # performing an infinite loop
 # for the window to display
